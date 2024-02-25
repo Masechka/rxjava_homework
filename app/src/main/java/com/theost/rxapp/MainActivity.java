e8ac4ffd72765e80d41c7d22d1151c77ec2b561f
Masechka <kleyzermp@gmail.com>
2024-02-25 21:51:01 +0300

    remake

--- app/src/main/java/com/theost/rxapp/MainActivity.java
+++ app/src/main/java/com/theost/rxapp/MainActivity.java
@@ -16,9 +16,12 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableSource;
+import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,60 +29,61 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private String text;
     private TextView textView;
+    private final Scheduler.Worker worker = Schedulers.computation().createWorker();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
 
         binding.button.setOnClickListener(v -> {
+            Log.i("INSIDE CLICK", "Click!");
             binding.progressBar.setVisibility(View.VISIBLE);
             binding.button.setVisibility(View.GONE);
-            editText = (EditText) findViewById(R.id.editText);
             text = editText.getText().toString().trim();
-            textView = (TextView) findViewById(R.id.textView);
-            new LoadingThread().start();
-        });
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-
-    class LoadingThread extends Thread {
-        @Override
-        public void run() {
 
             Log.wtf("RXJAVA", "disposableMap -----");
-            Disposable disposableMap = Api.getData()
-                    .map(values ->
-                            values
-                                    .stream()
-                                    .map(ApiObject::getValue)
-                                    .sorted((first, second) -> Integer.compare(- ((first.length() - first.replace(text, "").length()) / text.length()), - ((second.length() - second.replace(text, "").length()) / text.length())))
-                                    .limit(100)
-                                    .sorted()
-                                    .collect(Collectors.toList())
-                    )
-                    .subscribe(
+
+            worker.schedule(this::Rx);
+            }
+        );
+    }
+
+
+    void Rx() {
+        Disposable disposableMap = Api.getData()
+                .map(values -> values
+                        .stream()
+                        .map(ApiObject::getValue)
+                        .sorted((first, second) -> Integer.compare(-((first.length() - first.replace(text, "").length()) / text.length()), -((second.length() - second.replace(text, "").length()) / text.length())))
+                        .limit(100)
+                        .sorted()
+                        .collect(Collectors.toList())
+                )
+                .subscribe(
                             res -> {
                                 Log.wtf("RXJAVA", "Result: " + String.join("; ", res));
                                 runOnUiThread(() -> {
                                     textView.setText(String.join("; ", res));
                                     binding.progressBar.setVisibility(View.GONE);
                                     binding.button.setVisibility(View.VISIBLE);
-                                });
-                            },
+                                });},
                             throwable ->
                                     Log.wtf("RXJAVA", "onError: " + throwable),
                             () -> {
                                 Log.wtf("RXJAVA", "onComplete");
+                                worker.dispose();
                             }
-                    );
-        }
+                        );
     }
-}
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }}
+
+
